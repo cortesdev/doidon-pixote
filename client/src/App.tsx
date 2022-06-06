3f97dcf2d503a6f6cf6d5d2e1ed24e0a19d1167c
Ricardo Côrtes <giralander@hotmail.com>
2022-06-06 19:16:37 +0200

    Added server

---
+++ client/src/App.tsx
@@ -0,0 +1,57 @@
+import { FC, useEffect, useState } from 'react';
+import { Router } from './pages/Router';
+import { theme } from './styles/theme';
+import { ThemeProvider } from '@mui/material';
+import './styles/globals.css';
+import './styles/mui-overrides.css';
+import { Loader } from './design-system/_common/Loader';
+
+
+interface AppProps {
+  name?: any
+  children?: Element
+  ebconfig?: any
+  options?: any
+}
+
+
+
+const App: FC<AppProps> = ({ children,
+  ebconfig,
+  options }) => {
+
+
+
+
+  const [data, setData] = useState(false);
+
+
+  const setDataTimeout = () => {
+    setTimeout(() => {
+      setData(true);
+    }, 3500);
+  }
+
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+
+      setDataTimeout()
+
+  }, []);
+
+
+  return (
+    <div className="App">
+
+      <ThemeProvider theme={theme}>
+        <Router />
+        {!data ?  <Loader /> : ''}
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
