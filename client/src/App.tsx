293fa60518646c68fb28486bf6bc137017be9904
Ricardo Côrtes <giralander@hotmail.com>
2022-06-07 11:13:45 +0200

    Added api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,9 @@ import './styles/globals.css';
 import './styles/mui-overrides.css';
 import { Loader } from './design-system/_common/Loader';
 
+import { useGetPosts } from './lib/api-hooks';
+import { FetchState } from './types';
+
 
 interface AppProps {
   name?: any
@@ -19,6 +22,7 @@ interface AppProps {
 const App: FC<AppProps> = ({ children,
   ebconfig,
   options }) => {
+    const [posts, fetchState, getPosts] = useGetPosts();
 
 
 
@@ -29,6 +33,7 @@ const App: FC<AppProps> = ({ children,
   const setDataTimeout = () => {
     setTimeout(() => {
       setData(true);
+      getPosts()
     }, 3500);
   }
 
@@ -49,6 +54,39 @@ const App: FC<AppProps> = ({ children,
       <ThemeProvider theme={theme}>
         <Router />
         {!data ?  <Loader /> : ''}
+
+        {fetchState === FetchState.DEFAULT && (
+        <>
+          <p>
+            Hello there, click the button below to get the list of posts from
+            the API.
+          </p>
+          <button onClick={getPosts}>Get Posts</button>
+        </>
+      )}
+      {fetchState === FetchState.LOADING && <p>Fetching posts...</p>}
+      {fetchState === FetchState.ERROR && (
+        <>
+          <p>Oops! Something went wrong. Please try again.</p>
+          <button onClick={getPosts}>Get Posts</button>
+        </>
+      )}
+      {fetchState === FetchState.SUCCESS && (
+        <>
+          <p>Here's the list of posts:</p>
+          <ul className="posts-list">
+            {posts.map((post) => (
+              <li key={post.id} className="post">
+                <h3>
+                  {post.id} - {post.title}
+                </h3>
+                <p>{post.body}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+ 
       </ThemeProvider>
     </div>
   );
